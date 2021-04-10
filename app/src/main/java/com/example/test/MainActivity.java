914b03d93c850b5a778184b74850c03c805ae572
alex <antepa.alex04@gmail.com>
2021-04-10 15:43:07 +0800

    new

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    ListView list;
+    Button mapButton, lentaButton;
+    FloatingActionButton addButton;
+    SimpleAdapter simpleAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapButton = findViewById(R.id.mapButton);
+        lentaButton = findViewById(R.id.lentaButton);
+        addButton = findViewById(R.id.addButton);
+        list = findViewById(R.id.list);
+
+        lentaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Good", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Goooooooooood", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //simpleAdapter = new SimpleAdapter(this, )
     }
 }
